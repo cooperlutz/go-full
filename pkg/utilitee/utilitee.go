fb92db7cfcb5bf8bf22e37e64660db830257535f
cooperlutz <cooperblutz@yahoo.com>
2026-01-12 16:32:15 -0600

    fix: add utility functions

--- pkg/utilitee/utilitee.go
+++ pkg/utilitee/utilitee.go
@@ -1,6 +1,9 @@
 package utilitee
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 // RightNow is a simple utility function to ensure consistency of returning a "now" timestamp being localized to UTC.
 func RightNow() time.Time {
@@ -24,4 +27,16 @@ func TimePtr(t time.Time) *time.Time {
 
 func IntPtr(i int) *int {
 	return &i
+}
+
+func SafeIntToInt32(i *int) int32 {
+	if i == nil {
+		return 0
+	}
+	// Check for overflow/underflow
+	if *i > math.MaxInt32 || *i < math.MinInt32 {
+		return 0
+	}
+
+	return int32(*i)
 }
