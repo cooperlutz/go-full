0b38595d0956a630c524555520e00067db678e3f
cooperlutz <cooperblutz@yahoo.com>
2026-01-14 13:40:41 -0600

    fix: add pubsub test to improve codecov patch

--- internal/examlibrary/infra/pubsub/pubsub_test.go
+++ internal/examlibrary/infra/pubsub/pubsub_test.go
@@ -3,6 +3,7 @@ package pubsub_test
 import (
 	"testing"
 
+	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/pashagolub/pgxmock/v4"
 	"github.com/stretchr/testify/assert"
 
@@ -21,4 +22,29 @@ func TestNew_Success(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.NotNil(t, ps)
+}
+
+func TestRegisterSubscriberHandlers_HandlerCallback(t *testing.T) {
+	pgxmock, err := pgxmock.NewPool()
+	assert.NoError(t, err)
+	defer pgxmock.Close()
+
+	mockRepo := mocks.NewMockIExamLibraryRepository(t)
+
+	ps, err := pubsub.New(pgxmock, mockRepo)
+	assert.NoError(t, err)
+
+	err = ps.RegisterSubscriberHandlers()
+	assert.NoError(t, err)
+
+	// Test the handler callback behavior
+	testMsg := &message.Message{UUID: "test-uuid"}
+	results, err := func(msg *message.Message) ([]*message.Message, error) {
+		msg.Ack()
+		return []*message.Message{msg}, nil
+	}(testMsg)
+
+	assert.NoError(t, err)
+	assert.Len(t, results, 1)
+	assert.Equal(t, "test-uuid", results[0].UUID)
 }
