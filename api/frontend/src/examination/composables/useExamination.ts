27bf532ca10c58e79e4edbb6f1a10d49757b178d
cooperlutz <cooperblutz@yahoo.com>
2026-01-29 14:47:38 -0600

    fix: add examination composables to frontend

--- api/frontend/src/examination/composables/useExamination.ts
+++ api/frontend/src/examination/composables/useExamination.ts
@@ -6,12 +6,16 @@ import {
   type Exam,
   type StartExam,
   type StartNewExamRequest,
+  type Question,
+  type GetExamQuestionRequest,
+  type Progress,
+  type GetExamRequest,
+  type SubmitExamRequest,
 } from "~/examination/services";
 
 const examinationAPI = new DefaultApi(BackendConfig);
 
-// Composable for sending a ping pong message
-export function useExamination() {
+export function useStartExam() {
   const error = ref<Error | null>(null);
   const loading = ref(false);
   const exam = ref<Exam | null>(null);
@@ -44,5 +48,162 @@ export function useExamination() {
     loading,
     startExam,
     exam,
+  };
+}
+
+export function useSubmitExam() {
+  const error = ref<Error | null>(null);
+  const loading = ref(false);
+
+  const submitExam = async (examId: string) => {
+    loading.value = true;
+    error.value = null;
+    const req: SubmitExamRequest = {
+      examId,
+    };
+    try {
+      await examinationAPI.submitExam(req);
+    } catch (err) {
+      if (err instanceof Error) {
+        error.value = err;
+      }
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  return {
+    error,
+    loading,
+    submitExam,
+  };
+}
+
+export function useGetExam() {
+  const error = ref<Error | null>(null);
+  const loading = ref(false);
+  const exam = ref<Exam | null>(null);
+
+  const getExam = async (examId: string) => {
+    loading.value = true;
+    error.value = null;
+    const req: GetExamRequest = {
+      examId,
+    };
+    try {
+      const response = await examinationAPI.getExam(req);
+      exam.value = response;
+      return response;
+    } catch (err) {
+      if (err instanceof Error) {
+        error.value = err;
+      }
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  return {
+    error,
+    loading,
+    getExam,
+    exam,
+  };
+}
+
+export function useExaminationProgress() {
+  const error = ref<Error | null>(null);
+  const loading = ref(false);
+  const progress = ref<Progress | null>(null);
+
+  const getExamProgress = async (examId: string) => {
+    loading.value = true;
+    error.value = null;
+    try {
+      const response = await examinationAPI.getExamProgress({ examId });
+      progress.value = response;
+      return response;
+    } catch (err) {
+      if (err instanceof Error) {
+        error.value = err;
+      }
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  return {
+    error,
+    loading,
+    getExamProgress,
+    progress,
+  };
+}
+
+export function useGetQuestion() {
+  const error = ref<Error | null>(null);
+  const loading = ref(false);
+  const examQuestion = ref<Question | null>(null);
+
+  const getQuestion = async (examId: string, index: number) => {
+    loading.value = true;
+    error.value = null;
+    const req: GetExamQuestionRequest = {
+      examId,
+      questionIndex: index,
+    };
+    try {
+      const response = await examinationAPI.getExamQuestion(req);
+      examQuestion.value = response;
+      return response;
+    } catch (err) {
+      if (err instanceof Error) {
+        error.value = err;
+      }
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  return {
+    error,
+    loading,
+    getQuestion,
+    examQuestion,
+  };
+}
+
+export function useSubmitAnswer() {
+  const error = ref<Error | null>(null);
+  const loading = ref(false);
+
+  const submitAnswer = async (
+    examId: string,
+    questionIndex: number,
+    answer: string,
+  ) => {
+    loading.value = true;
+    error.value = null;
+    try {
+      await examinationAPI.answerQuestion({
+        examId,
+        questionIndex,
+        answer: {
+          providedAnswer: answer,
+        },
+      });
+    } catch (err) {
+      if (err instanceof Error) {
+        error.value = err;
+      }
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  return {
+    error,
+    loading,
+    submitAnswer,
   };
 }
