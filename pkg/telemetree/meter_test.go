15b884d152ea0cfec75e885fb4677cdf74c0d9c3
Cooper Lutz <cooperblutz@yahoo.com>
2026-02-14 17:04:56 -0600

    fix: update resource def and conf of telemetree (#172)

--- pkg/telemetree/meter_test.go
+++ pkg/telemetree/meter_test.go
@@ -1,8 +1,10 @@
 package telemetree_test
 
 import (
+	"context"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/otel/attribute"
 
@@ -16,21 +18,22 @@ func TestInitMeter(t *testing.T) {
 	// Set config values for resource definition
 	config.ApplicationName = "test-service"
 	config.ApplicationVersion = "2.0.0"
+	config.ApplicationInstanceID = uuid.New().String()
 
-	mp, err := telemetree.InitMeter()
+	mp, err := telemetree.InitMeter(context.Background())
 	assert.NoError(t, err)
 	assert.NotNil(t, mp)
 
 	// Check that the MeterProvider's resource has expected attributes
 	// Since MeterProvider does not expose Resource(), test the resource via ResourceDefinition
-	rd, err := telemetree.ResourceDefinition()
+	rd, err := telemetree.ResourceDefinition(context.Background())
 	assert.NoError(t, err)
 	attrs := rd.Attributes()
 
 	expected := []attribute.KeyValue{
 		attribute.String("service.name", "test-service"),
 		attribute.String("service.version", "2.0.0"),
-		attribute.String("service.instance.id", "unique-instance-id"),
+		attribute.String("service.instance.id", config.ApplicationInstanceID),
 	}
 
 	// Assert
